1cebdf6ff398235e10f0354d9a9232074367b13b
Florian-PC <48243528+Florian-PC@users.noreply.github.com>
2019-03-05 12:35:46 +0100

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/Florian-PC/repository.git

--- ElasticGroup/src/service/ServiceMagasin.java
+++ ElasticGroup/src/service/ServiceMagasin.java
@@ -3,24 +3,4 @@ package service;
 public class ServiceMagasin {
 	
 	
-	//creater une list de x Article null ou x est le nombre d'article du SELECT d'oracle
-
-	public 
-	Service.getSessionBeanBrowser(request).getListBlocFiltre()
-	 for (BlocFiltre indiceBlocFiltre : listBlocFiltre) {
-	
-	List<Filtre> listFiltreFamille = new ArrayList<Filtre>();
-	for (Filtre indiceFiltre : BlocFiltre.getListFiltre()) {
-		if(indiceFiltre.ischecked) {
-			listFiltreFamille.add(indiceFiltre);
-		}
-	}	
-	if (listFiltreFamille == null){
-		listFiltreFamille = ;
-	}
-	 }
-	
-	
-	
-	
 }
